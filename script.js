8490715449f707d8232fd8d7c7f65f5746621e93
Reshy <reshy@protonmail.com>
2021-07-26 14:17:36 +0200

    Menu popup hotfix

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ function menuToggle() {
     let div = document.querySelector('#main-nav-bar');
     let div2 = document.querySelector('#menu-js');
     div.classList.toggle('js-toggle');
-    div2.classList.replace('main-menu','js-main-menu');
+    div2.classList.toggle('js-main-menu');
+    div2.classList.toggle('main-menu');
 }
