2a3cd8e4c748e901b432a473496805ec7037d005
rpire <rubenpire7@gmail.com>
2021-07-26 09:38:52 -0400

    Implement toggle function to the main menu

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function menuToggle() {
-    alert('hello');
-    let div = document.querySelector('#main-nav-bar');
-    let div2 = document.querySelector('#menu-js');
-    div.classList.toggle('js-toggle');
-    div2.classList.toggle('js-main-menu');
-    div2.classList.toggle('main-menu');
+    var mq = window.matchMedia( "(max-width: 1100px)");
+    if (mq.matches) {
+        let div = document.querySelector('#main-nav-bar');
+        let div2 = document.querySelector('#menu-js');
+        let burger = document.querySelector('#burger-container');
+        let cross = document.querySelector('#cross');
+        let shapes = document.querySelector('#shape-toggle');
+        div.classList.toggle('js-toggle');
+        div2.classList.toggle('js-main-menu');
+        div2.classList.toggle('main-menu');
+        burger.classList.toggle('invisible');
+        cross.classList.toggle('invisible');
+        shapes.classList.toggle('invisible');
+    }
 }
