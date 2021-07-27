68864aa4e085fc76dc6341b1f2919d2eee57bc16
rpire <rubenpire7@gmail.com>
2021-07-27 09:49:39 -0400

    Implement functinality to the modal pop-up

---
+++ modal.js
@@ -0,0 +1,86 @@
+const cardImages = './img/laptop'
+
+const card1 = {
+    title: '1 Project',
+    technologies: [
+      'HTML/CSS',
+      'Ruby on Rails',
+      'JavaScript'
+    ],
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ullam modi architecto reprehenderit officiis consequatur id molestias sint, maxime cumque ad excepturi atque voluptatem nihil delectus placeat recusandae natus exercitationem!',
+    link: 'http://www.github.com'
+  };
+
+  const card2 = {
+    title: '2 Project',
+    technologies: [
+      'HTML/CSS',
+      'Ruby on Rails',
+      'JavaScript'
+    ],
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ullam modi architecto reprehenderit officiis consequatur id molestias sint, maxime cumque ad excepturi atque voluptatem nihil delectus placeat recusandae natus exercitationem!',
+    link: 'http://www.google.com'
+  };
+
+  const card3 = {
+    title: '3 Project',
+    technologies: [
+      'HTML/CSS',
+      'Ruby on Rails',
+      'JavaScript'
+    ],
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ullam modi architecto reprehenderit officiis consequatur id molestias sint, maxime cumque ad excepturi atque voluptatem nihil delectus placeat recusandae natus exercitationem!',
+    link: 'https://github.com/rpire/microverse-portfolio/tree/modal'
+  };
+
+  const card4 = {
+    title: '4 Project',
+    technologies: [
+      'HTML/CSS',
+      'Ruby on Rails',
+      'JavaScript'
+    ],
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ullam modi architecto reprehenderit officiis consequatur id molestias sint, maxime cumque ad excepturi atque voluptatem nihil delectus placeat recusandae natus exercitationem!',
+    link: 'https://github.com/rpire/microverse-portfolio/tree/modal'
+  };
+
+  const card5 = {
+    title: '5 Project',
+    technologies: [
+      'HTML/CSS',
+      'Ruby on Rails',
+      'JavaScript'
+    ],
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ullam modi architecto reprehenderit officiis consequatur id molestias sint, maxime cumque ad excepturi atque voluptatem nihil delectus placeat recusandae natus exercitationem!',
+    link: 'https://github.com/rpire/microverse-portfolio/tree/modal'
+  };
+
+  const projects = [card1, card2, card3, card4, card5];
+
+  function modal(number){
+    const card = document.querySelector('#card');
+    card.classList.toggle('invisible');
+    let title = document.querySelector("#card-title");
+    let list = document.querySelector("#card-list");
+    list.innerHTML = '';
+    let img = document.querySelector(".card-image");
+    let desc = document.querySelector("#card-description");
+    let buttonsrc = document.querySelector("#card-button-source");
+    let buttonlive = document.querySelector("#card-button-live");
+
+    title.innerHTML = projects[number].title;
+
+    for (let i=0;i<projects[number].technologies.length;i++) {
+        let li = document.createElement('li');
+	      li.textContent = projects[number].technologies[i];
+	      list.appendChild(li);
+      }
+
+    img.setAttribute("src", cardImages+(number+1)+"-big.png");
+    buttonsrc.setAttribute("onclick", "window.open('"+projects[number].link+"', '_blank');");
+    buttonlive.setAttribute("onclick", "window.open('"+projects[number].link+"', '_blank');");
+    desc.innerHTML = projects[number].description;
+};
+function toggle(){
+  card.classList.toggle('invisible');
+}
